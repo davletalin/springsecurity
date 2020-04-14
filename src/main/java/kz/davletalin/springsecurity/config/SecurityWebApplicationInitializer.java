7b1edbee46f6e8971d3532739077ae19b2816860
Armat <davletalin@gmail.com>
2020-04-14 20:34:46 +0600

    add users in memory authentication

---
+++ src/main/java/kz/davletalin/springsecurity/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package kz.davletalin.springsecurity.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
